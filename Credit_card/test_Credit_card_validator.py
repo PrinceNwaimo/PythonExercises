4978d60110841c0804bfd765d5e34d2a2e036dc0
Prince Nwaimo <nwaimoprince12@gmail.com>
2023-01-27 08:30:25 +0100

    "added credit card validator and test"

---
+++ Credit_card/test_Credit_card_validator.py
@@ -0,0 +1,13 @@
+import unittest
+from Credit_card_validator import even_double_digits
+
+
+class TestCredit_card_validator(unittest.TestCase):
+
+    def test_even_double_digits(self):
+        self.assertEqual(even_double_digits("5399834432138592"), 49)
+        self.assertEquals(even_double_digits("12324342727"), 22)
+
+
+if __name__ == '__main__':
+    unittest.main()
